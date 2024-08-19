58a08677d982f75b6efa05f3756b3ce2c142da5c
Brijesh <brijesh@Brijeshs-MacBook-Air.local>
2024-08-19 19:37:11 +0530

    fix(checkout): hadle clear cart when unmount checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,30 +1,20 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { useCart } from "@/lib/context/cart/context";
-import { useRouter } from "next/navigation";
 
 const Checkout: React.FC = () => {
   const { cart, dispatch } = useCart();
-  const componentWillUnmount = useRef(false);
-  const router = useRouter();
 
   useEffect(() => {
-    if (cart.length === 0) {
-      router.push("/products");
-    }
-
     return () => {
-      if (componentWillUnmount.current) {
-        dispatch({ type: "CLEAR_CART" });
-      }
-      componentWillUnmount.current = true;
+      dispatch({ type: "CLEAR_CART" });
     };
-  }, [router]);
+  }, [dispatch]);
 
   return (
     <div className="text-center py-8 px-4">
-      {cart.length > 0 && (
+      {cart.length > 0 ? (
         <div className="bg-white shadow-md rounded-lg border border-gray-200 p-6 mx-auto max-w-md">
           <h2 className="text-2xl font-bold mb-6">Your order</h2>
           <h3 className="text-xl font-semibold mb-4">
@@ -36,6 +26,15 @@ const Checkout: React.FC = () => {
           </p>
           <Link href="/products">
             <button className="w-full px-4 py-2 bg-primary text-white rounded hover:bg-blue-600 transition-colors">
+              Continue Shopping
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div className="text-center py-8">
+          <h2 className="text-2xl font-bold mb-4">Your cart is empty</h2>
+          <Link href="/products">
+            <button className="px-4 py-2 bg-primary text-white rounded hover:bg-blue-600 transition-colors">
               Continue Shopping
             </button>
           </Link>
